22bfb0c5868df83bb629732f1f87b13750d702df
Ravindu Janananda <ravindujanananda180@gmail.com>
2025-04-07 23:04:38 +0530

    update

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/vs code/IDF/Smart-Fish-Tank-with-automated-feeder/Smart-Fish-Tank-with-automated-feeder/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/ravin/esp/v5.4.1/esp-idf/.git")
+set(GIT_DIR "D:/vs code/IDF/Smart-Fish-Tank-with-automated-feeder/Smart-Fish-Tank-with-automated-feeder/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
